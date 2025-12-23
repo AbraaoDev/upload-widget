403c9e75f759e23ca5af3526f4ee09d8725b4f7b
abraaodev <abraaohomualdo@gmail.com>
2025-12-23 18:45:57 -0300

    feat: animate components with motion

--- web/src/components/upload-widget-dropzone.tsx
+++ web/src/components/upload-widget-dropzone.tsx
@@ -1,7 +1,12 @@
 import { useDropzone } from 'react-dropzone'
+import CircularProgressBar from './ui/circular-progress-bar'
+import { motion } from 'motion/react'
 
 
 export function UploadWidgetDropzone() {
+  const isThereAnyPendingUpload = true
+  const uploadGlobalPercentage = 66
+
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     multiple: true,
     accept: {
@@ -13,15 +18,30 @@ export function UploadWidgetDropzone() {
     },
   })
   return (
-    <div className="px-3 flex flex-col gap-3" >
+    <motion.div className="px-3 flex flex-col gap-3"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.2 }}
+    >
       <div {...getRootProps()}
         data-active={isDragActive}
         className='cursor-pointer text-zinc-400 bg-black/20 rounded-lg border border-zinc-700 border-dashed h-32 flex flex-col items-center justify-center gap-1 transition-colors hover:border-zinc-600 data-[active=true]:bg-emerald-500/10 data-[active=true]:border-emerald-500 data-[active=true]:text-emerald-400'>
         <input type='file' {...getInputProps()} />
-        <span className='text-xs'>Drop your files here or</span>
-        <span className='text-xs underline'>click to open picker</span>
-      </div>
-      <span className='text-xs text-zinc-400'>Only PNG and JPG files are supported</span>
-    </div>
+        {isThereAnyPendingUpload
+          ? (
+            <div className="flex flex-col gap-2.5 items-center">
+              <CircularProgressBar progress={uploadGlobalPercentage} size={56} strokeWidth={4} />
+              <span className='text-xs'>Uploading 2 files...</span>
+            </div>
+          ) : (
+            <>
+              <span className='text-xs'>Drop your files here or</span>
+              <span className='text-xs underline'>click to open picker</span>
+            </>
+          )
+        }
+      </div >
+      <span className='text-xxs text-zinc-400'>Only PNG and JPG files are supported</span>
+    </motion.div >
   )
 }
--- web/src/components/upload-widget.tsx
+++ web/src/components/upload-widget.tsx
@@ -1,15 +1,37 @@
-import { useState } from "react";
 import { UploadWidgetDropzone } from "./upload-widget-dropzone";
 import { UploadWidgetHeader } from "./upload-widget-header";
 import { UploadWidgetList } from "./upload-widget-list";
 import * as Collapsible from '@radix-ui/react-collapsible'
 import { UploadWidgetMinimizedButton } from "./upload-widget-minimized-button";
+import { motion, useCycle } from 'motion/react'
 
 export function UploadWidget() {
-  const [isWidgetOpen, setIsWidgetOpen] = useState(false);
+  const isThereAnyPendingUpload = true
+  const [isWidgetOpen, toggleWidgetOpen] = useCycle(false, true);
   return (
-    <Collapsible.Root onOpenChange={setIsWidgetOpen}>
-      <div className="bg-zinc-900 overflow-hidden w-90 rounded-xl shadow-shape">
+    <Collapsible.Root onOpenChange={() => toggleWidgetOpen()}>
+      <motion.div
+        data-state={isWidgetOpen ? 'open' : 'closed'}
+        data-progress={isThereAnyPendingUpload}
+        className="bg-zinc-900 overflow-hidden w-90 rounded-xl data-[state=open]:shadow-shape border border-transparent animate-border data-[state=closed]:rounded-3xl data-[state=closed]:data-[progress=false]:shadow-shape  data-[state=closed]:data-[progress=true]:[background:linear-gradient(45deg,#09090B,theme(colors.zinc.900)_50%,#09090B)_padding-box,conic-gradient(from_var(--border-angle),theme(colors.zinc.700/.48)_80%,_theme(colors.indigo.500)_86%,_theme(colors.indigo.300)_90%,_theme(colors.indigo.500)_94%,_theme(colors.zinc.600/.48))_border-box]"
+        animate={isWidgetOpen ? 'open' : 'closed'}
+        variants={{
+          closed: {
+            width: 'max-content',
+            height: 44,
+            transition: {
+              type: 'inertia'
+            }
+          },
+          open: {
+            width: 360,
+            height: 'auto',
+            transition: {
+              duration: 0.1
+            }
+
+          }
+        }}>
         {!isWidgetOpen && <UploadWidgetMinimizedButton />}
         <Collapsible.Content>
           <UploadWidgetHeader />
@@ -18,7 +40,7 @@ export function UploadWidget() {
             <div className="h-px bg-zinc-800 border-t border-black/50 box-content" />
             <UploadWidgetList />
           </div></Collapsible.Content>
-      </div>
+      </motion.div>
     </Collapsible.Root>
   )
 }
